cf58bcda2fdeb5312b09f5bf26eed236570f406b
Rishin05 <rishinp26@gmail.com>
2025-01-26 18:16:07 -0500

    did some changes for prod

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,25 @@
 "use client";
-
 import React, { useEffect } from "react";
 import LoginForm from "../Components/auth/LoginForm/LoginForm";
 import { useUserContext } from "@/context/userContext";
 import { useRouter } from "next/navigation";
 
 function page() {
-  const {user} = useUserContext();
+  const { user } = useUserContext();
   const router = useRouter();
 
   useEffect(() => {
-    if(user && user._id) {
+    // redirect to home page if user is already logged in
+    if (user && user._id) {
       router.push("/");
     }
   }, [user, router]);
 
-  if(user && user._id) {
+  // return null or a loading spinner/indicator
+  if (user && user._id) {
     return null;
   }
+
   return (
     <div className="auth-page w-full h-full flex justify-center items-center">
       <LoginForm />
